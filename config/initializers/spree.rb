230e9c0a593d0c6801d7ed62848e8aebfd8402f2
Boris Lopez <Boris_Lopez@gap.com>
2016-03-02 00:30:57 -0300

    temporary logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -27,7 +27,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus_logo.png"
+  config.logo = "logo/logia-bahut.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
