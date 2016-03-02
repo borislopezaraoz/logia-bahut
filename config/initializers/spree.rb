f6a7fcd0710346d5ab4ab7c441e8b7581102f3e3
Boris Lopez <Boris_Lopez@gap.com>
2016-03-02 00:05:02 -0300

    locale -> español

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,7 +10,7 @@ Spree.config do |config|
   # Core:
 
   # Default currency for new sites
-  config.currency = "USD"
+  config.currency = "CLP"
 
   # from address for transactional emails
   config.mails_from = "store@example.com"
@@ -54,13 +54,13 @@ end
 Spree::Frontend::Config.configure do |config|
   config.use_static_preferences!
 
-  config.locale = 'en'
+  config.locale = 'es'
 end
 
 Spree::Backend::Config.configure do |config|
   config.use_static_preferences!
 
-  config.locale = 'en'
+  config.locale = 'es'
 end
 
 Spree::Api::Config.configure do |config|
